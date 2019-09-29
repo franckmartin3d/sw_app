49b7f8a263c4b7eddc42542a026f4d520418829f
franckmartin3d <franck.martin3d@gmail.com>
2019-09-29 23:25:23 +1000

    can see website

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("app ran!");
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/static/index.html')))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
