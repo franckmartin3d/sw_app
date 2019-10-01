95b6901e64a3e1ab6f6ada5a5fabd458a452d2e6
Francois Martin <franck.martin3d@gmail.com>
2019-10-01 17:23:27 +1000

    internal api working

--- static/script.js
+++ static/script.js
@@ -1,51 +1,31 @@
 console.log("script is running");
 
-const character = {
 
-    "name": "Luke Skywalker",
-    "height": "172",
-    "mass": "77",
-    "hair_color": "blond",
-    "skin_color": "fair",
-    "eye_color": "blue",
-    "birth_year": "19BBY",
-    "gender": "male",
-    "homeworld": "https://swapi.co/api/planets/1/",
-    "films": [
-        "https://swapi.co/api/films/2/",
-        "https://swapi.co/api/films/6/",
-        "https://swapi.co/api/films/3/",
-        "https://swapi.co/api/films/1/",
-        "https://swapi.co/api/films/7/"
-    ],
-    "species": [
-        "https://swapi.co/api/species/1/"
-    ],
-    "vehicles": [
-        "https://swapi.co/api/vehicles/14/",
-        "https://swapi.co/api/vehicles/30/"
-    ],
-    "starships": [
-        "https://swapi.co/api/starships/12/",
-        "https://swapi.co/api/starships/22/"
-    ],
-    "created": "2014-12-09T13:50:51.644000Z",
-    "edited": "2014-12-20T21:17:56.891000Z",
-    "url": "https://swapi.co/api/people/1/"
-}
 
 function displayCharacter(){
-    const unorderedList = document.getElementById('Skywalker-list');
 
-    const properties = Object.keys(character)
+    fetch('/api/character')
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) {
+      console.log(data);
+
+      const unorderedList = document.getElementById('Skywalker-list');
+
+      const properties = Object.keys(data)
+      
+      properties.forEach(function(property){
+          const listItemEl = document.createElement("LI");
+          const textNode = document.createTextNode(property + ": " + data[property]);
+          listItemEl.appendChild(textNode);
+  
+          unorderedList.appendChild(listItemEl);
+      });
+
+      });
     
-    properties.forEach(function(property){
-        const listItemEl = document.createElement("LI");
-        const textNode = document.createTextNode(property + ": " + character[property]);
-        listItemEl.appendChild(textNode);
 
-        unorderedList.appendChild(listItemEl);
-    });
+
+    
 
 
 }
