5142c2ad63d7bfd6ec30336295590342abd3ccb8
Francois Martin <franck.martin3d@gmail.com>
2019-10-01 17:47:40 +1000

    External Fetch done

--- server.js
+++ server.js
@@ -1,50 +1,66 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path');
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const axios = require("axios");
 
-app.use('/static', express.static('static'));
+app.use("/static", express.static("static"));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/static/index.html')))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname + "/static/index.html"))
+);
 
-app.get('/api/character', function(req,res) {
-    res.send(character)
+app.get("/api/character", function(req, res) {
+  // res.send(character)
+
+  //Get data from swapi.co
+  axios
+    .get("https://swapi.co/api/people/3")
+    .then(function(response) {
+      // handle success
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    });
+
+    
 });
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// const character = {
 
-
-const character = {
-
-    "name": "Luke Skywalker",
-    "height": "172",
-    "mass": "77",
-    "hair_color": "blond",
-    "skin_color": "fair",
-    "eye_color": "blue",
-    "birth_year": "19BBY",
-    "gender": "male",
-    "homeworld": "https://swapi.co/api/planets/1/",
-    "films": [
-        "https://swapi.co/api/films/2/",
-        "https://swapi.co/api/films/6/",
-        "https://swapi.co/api/films/3/",
-        "https://swapi.co/api/films/1/",
-        "https://swapi.co/api/films/7/"
-    ],
-    "species": [
-        "https://swapi.co/api/species/1/"
-    ],
-    "vehicles": [
-        "https://swapi.co/api/vehicles/14/",
-        "https://swapi.co/api/vehicles/30/"
-    ],
-    "starships": [
-        "https://swapi.co/api/starships/12/",
-        "https://swapi.co/api/starships/22/"
-    ],
-    "created": "2014-12-09T13:50:51.644000Z",
-    "edited": "2014-12-20T21:17:56.891000Z",
-    "url": "https://swapi.co/api/people/1/"
-}
+//     "name": "Luke Skywalker",
+//     "height": "172",
+//     "mass": "77",
+//     "hair_color": "blond",
+//     "skin_color": "fair",
+//     "eye_color": "blue",
+//     "birth_year": "19BBY",
+//     "gender": "male",
+//     "homeworld": "https://swapi.co/api/planets/1/",
+//     "films": [
+//         "https://swapi.co/api/films/2/",
+//         "https://swapi.co/api/films/6/",
+//         "https://swapi.co/api/films/3/",
+//         "https://swapi.co/api/films/1/",
+//         "https://swapi.co/api/films/7/"
+//     ],
+//     "species": [
+//         "https://swapi.co/api/species/1/"
+//     ],
+//     "vehicles": [
+//         "https://swapi.co/api/vehicles/14/",
+//         "https://swapi.co/api/vehicles/30/"
+//     ],
+//     "starships": [
+//         "https://swapi.co/api/starships/12/",
+//         "https://swapi.co/api/starships/22/"
+//     ],
+//     "created": "2014-12-09T13:50:51.644000Z",
+//     "edited": "2014-12-20T21:17:56.891000Z",
+//     "url": "https://swapi.co/api/people/1/"
+// }
--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-console.log("script is running");
 
 
 
